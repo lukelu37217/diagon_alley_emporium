6b6ab2d8abf9a1a9fa220bd053f784fd6b674a54
ZLU <zlu4@rrc.ca>
2025-08-02 15:06:57 -0500

    Implement product listing and detail pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,17 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show]
+
   def index
+    @products = Product.includes(:category, image_attachment: :blob).page(params[:page])
+    @categories = Category.all
   end
 
   def show
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
