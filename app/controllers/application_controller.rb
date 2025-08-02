aeed21cd18f95ee2c5935148751682936cbf79d2
ZLU <zlu4@rrc.ca>
2025-08-02 15:11:18 -0500

    Enhance application controller with cart count

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_current_cart
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number])
+  end
+
+  private
+
+  def set_current_cart
+    return unless user_signed_in?
+    
+    @current_cart_count = current_user.shopping_carts.sum(:quantity)
+  end
 end
