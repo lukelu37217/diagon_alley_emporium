f36a86bd4e72f1be8dd7acbfd5680497dd9b5469
ZLU <zlu4@rrc.ca>
2025-08-02 15:23:40 -0500

    Fix CORS configuration issue

--- config/initializers/application_config.rb
+++ config/initializers/application_config.rb
@@ -3,14 +3,6 @@ Rails.application.configure do
   # Security headers
   config.force_ssl = false # Set to true in production
   
-  # CORS configuration for API endpoints
-  config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
-  end if Rails.env.development?
-  
   # Session configuration
   config.session_store :cookie_store, key: '_diagon_alley_session'
   
