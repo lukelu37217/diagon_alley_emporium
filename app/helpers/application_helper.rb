cb5a1bc769685bdd9e4dfd0890e3f69de35a785c
ZLU <zlu4@rrc.ca>
2025-08-02 15:13:39 -0500

    Enhance application helper with utility methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+  def flash_class(flash_type)
+    case flash_type.to_s
+    when 'notice'
+      'alert-success'
+    when 'alert'
+      'alert-danger'
+    when 'warning'
+      'alert-warning'
+    else
+      'alert-info'
+    end
+  end
+
+  def page_title(title = nil)
+    if title
+      "#{title} | Diagon Alley Emporium"
+    else
+      "Diagon Alley Emporium - Magical Items & Wizarding Supplies"
+    end
+  end
+
+  def current_cart_count
+    return 0 unless user_signed_in?
+    @current_cart_count ||= current_user.shopping_carts.sum(:quantity)
+  end
+
+  def currency_format(amount)
+    number_to_currency(amount, unit: '$', precision: 2)
+  end
+
+  def truncate_text(text, length = 100)
+    truncate(text, length: length, separator: ' ')
+  end
 end
