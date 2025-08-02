e6fbcbcbd59b121b6fe4eb297fca7950df284d66
ZLU <zlu4@rrc.ca>
2025-08-02 15:10:28 -0500

    Implement categories pages and controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,17 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+
   def index
+    @categories = Category.includes(:products)
   end
 
   def show
+    @products = @category.products.includes(image_attachment: :blob)
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
